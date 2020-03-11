e398a2fdae24a6cda992e0d686474c73595dface
Nedret Çelik <nedretcelik97@gmail.com>
2020-03-11 17:58:01 +0300

    Registration and Support Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom"; // npm install react-router-dom ı terminale yaz.
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import SideBar from './components/sidebar/SideBar';
+import Content from './components/content/Content';
+import { BrowserRouter as Router } from "react-router-dom";
+import Footer from './components/Footer'
 
-import Registration from './components/Registration'; 
+// Çalıştırmak içişn yükleme sırası:
+//** 1-) npm install 
+//2-) npm i -S @fortawesome/fontawesome-svg-core @fortawesome/react-fontawesome 
+//3-) npm i -S @fortawesome/free-light-svg-icons
+//4-) npm install reactstrap
+export default () => {
 
-function App() {
+  const [isOpen, setOpen] = useState(true)
+  const toggle = () => setOpen(!isOpen)
+
   return (
     <Router>
-      <Switch>
-        <Route path="/registration">
-          <Registration />
-        </Route>
-      </Switch>
+      <div className="App wrapper">
+        <SideBar toggle={toggle} isOpen={isOpen}/>
+        <Content toggle={toggle} isOpen={isOpen}/>
+      </div>
+      <Footer/>
     </Router>
   );
 }
-
-export default App;
