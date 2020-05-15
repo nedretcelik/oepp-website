ab1dc1aadc193a6c3e578e012fa7ff92f1604fe9
Nedret Çelik <nedretcelik97@gmail.com>
2020-05-16 00:20:47 +0300

    Updated for Final Project

--- API/app.js
+++ API/app.js
@@ -7,25 +7,45 @@ require("dotenv").config();
 const cors = require("cors");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
+const gameRoute = require('./routes/games');
+const contentRoute = require('./routes/contents');
+const quest = require('./question')
 const port = 3001;
+var MySQLStore = require('express-mysql-session')(session);
 
+app.use(cors({
+    origin: process.env.APP_URL,
+    credentials: true
+}));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-app.use(cors());
+var options = {
+	host: process.env.MYSQL_HOST,
+	port: 3306,
+	user: process.env.MYSQL_USER,
+	password: process.env.MYSQL_PASSWORD,
+	database: process.env.MYSQL_DATABASE
+};
+var sessionStore = new MySQLStore(options);
 
-app.use(session({ 
-    key: 'user_sid',
-    secret: 'somerandonstuffs',
-    resave: false, 
-    saveUninitialized: false, 
-    cookie: { expires: 6000000 } 
+app.use(session({
+	key: 'user_sid',
+	secret: 'somerandonstuffs',
+	store: sessionStore,
+    resave: true,
+    rolling: true,
+	saveUninitialized: false
 }));
 
 app.use("/user", userRoutes);
 app.use("/category",categoryRoutes);
+app.use("/games",gameRoute);
+app.use("/contents",contentRoute);
+app.use("/questions",quest);
+
 app.set("port", port);
 
 
--- API/routes/user.js
+++ API/routes/user.js
@@ -6,23 +6,20 @@ const sgMail = require('@sendgrid/mail');
 const mailgun = require("mailgun-js");
 const DOMAIN = process.env.DOMAIN_KEY;
 const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: DOMAIN});
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
+    connectionLimit: 100,
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
 });
 
-connection.connect(function(err) {
-    if (err) {
-        console.log("Error " + err);
-    }
-    console.log("Connected!");
-});
-
 router.post('/register', function(req,res) {
     const data = req.body;
     if(data.name !== "" && data.surname !== "" && data.email !== "" && data.username !== "" && data.password !== ""){
+        if(data.password !== data.confirmpassword){
+            return res.status(200).json({ status: 'error', message: "Password not matching!"});
+        }
         if(!validator.isEmail(data.email)){
             res.status(200).json({ status: 'error', message: "Not correct e-mail." });
         }else{
@@ -62,8 +59,6 @@ router.post('/register', function(req,res) {
     }
 });
 
-
-
 router.post('/login', function(req,res) {
     const data = req.body;
     if(data.username !== "" && data.password !== ""){
@@ -71,10 +66,14 @@ router.post('/login', function(req,res) {
             if(result.length>0) {
                 req.session.loggedin = true;
                 req.session.username = data.username;
+                req.session.UserID = result[0].UserID;
+                req.session.email = result[0].email;
+                req.session.name = result[0].name;
+                req.session.surname = result[0].surname;
                 res.status(200).json({ status: 'success', message: "Checked Data!"});
             }
             else{
-                res.status(200).json({ status: 'error', message: "Not Correct" });
+                res.status(200).json({ status: 'error', message: "Incorrect username/password" });
             }
         });
     }else{
@@ -91,7 +90,7 @@ router.post('/support', async function(req,res) {
 
         const emailMsg = {
             from: "OEPP <postmaster@sandboxb035355204c840d887be78db5f2d0bc2.mailgun.org>",
-            to: data.email,
+            to: "nedretcelik97@gmail.com",
             subject: data.subjectType,
             text: "This message from " + data.email + ". "  + data.message,
     };
@@ -104,18 +103,59 @@ router.post('/support', async function(req,res) {
     }
 });
 
-router.post('/forgot/password', function(req,res) {
+
+
+router.post('/feedback', async function(req,res) {
+    const data = req.body;
+    if (req.session === undefined || !req.session.loggedin) {
+        return res.status(401).send({ status: "error", message: "Not logged in" });
+    } else {
+        if(data.feedbackmessage !== "" && data.gameId !== undefined && data.rating !== undefined && req.session.UserID){
+            await connection.query('SELECT * FROM feedback WHERE feedbackFrom = ? AND gameId = ?', [req.session.UserID, data.gameId], async function(error,result,fields){
+                if(result.length>0) {
+                    return res.status(200).send({ status: 'error', message: "You can only submit one feedback for each course."});
+                }else{
+                    const sql = "INSERT INTO feedback (feedbackFrom, feedbackMessage, gameId, rating) VALUES ?";
+                    const values = [
+                        [req.session.UserID, data.feedbackmessage, data.gameId, data.rating]
+                    ];
+                    console.log("Inserting feedback.");
+                    await connection.query(sql, [values], async function (err, result) {
+                        if (err){
+                            console.log(err);
+                        }
+                    });
+
+                    // const emailMsg = {
+                    //     from: "OEPP <postmaster@sandboxb035355204c840d887be78db5f2d0bc2.mailgun.org>",
+                    //     to: data.email,
+                    //     text: "This feedback message from " + data.email + ". "  + data.feedbackmessage,
+                    //  };
+                    // mg.messages().send(emailMsg, function (error, body) {
+                    //     console.log(body);
+                    // });
+                    res.status(200).json({ status: 'success', message: "Feedback sent." });
+                }
+            })
+        }else{
+            res.status(200).json({ status: 'error', message: "Please enter feedback message."});
+        }
+    }
+});
+
+
+router.post('/forgot/password', async function(req,res) {
     const data = req.body;
     if(data.email !== ""){
         if(!validator.isEmail(data.email)){
             res.status(200).json({ status: 'error', message: "Not correct e-mail." });
         }else{
-            connection.query('SELECT * FROM user WHERE email = ?', [data.email], function(error,result,fields){
+            await connection.query('SELECT * FROM user WHERE email = ?', [data.email], async function(error,result,fields){
                 if(result.length>0) {
                     const sql = "UPDATE user SET passwordResetCode = ? WHERE email = ?";
                     console.log("Updating users.");
                     const resetCode = Math.random().toString().slice(2);
-                    connection.query(sql, [resetCode,data.email], async function (err, result) {
+                    await connection.query(sql, [resetCode,data.email], async function (err, result) {
                         if (err){
                             console.log(err);
                         }
@@ -173,5 +213,113 @@ router.post('/forgot/confirm/password', function(req,res) {
         });
     }
 });
+
+router.get('/logout', function(req,res) {
+    if(req.session){
+        req.session.destroy(function(error){
+            const logoutUrl = encodeURI(process.env.APP_URL + "/Login")
+            res.redirect(logoutUrl);
+        })
+    }
+});
+
+router.get('/profile', async function(req, res) {
+    if (req.session === undefined || !req.session.loggedin) {
+        return res.status(401).send({ status: "error", message: "Not logged in" });
+    } else {
+        connection.query('SELECT * FROM user WHERE UserID = ?', [req.session.UserID], function(error,result,fields){
+            if (result.length > 0) {
+                const retData = {
+                    name: result[0].name,
+                    surname: result[0].surname,
+                    email: result[0].email,
+                    username: result[0].username,
+                    password: result[0].password
+                };
+
+                return res.status(200).send({ status: "success", data: retData })
+            } else {
+                return res.status(200).send({ status: "error", message: "User not found" });
+            }
+        });
+    }
+})
+
+router.get('/profile/contents', async function(req, res) {
+    if (req.session === undefined || !req.session.loggedin) {
+        return res.status(401).send({ status: "error", message: "Not logged in" });
+    } else {
+        const contents = [];
+        await connection.query('SELECT c.*, g.* FROM Contents c LEFT JOIN GAMES g ON c.Gameid = g.idGames WHERE c.usr_id = ?', [req.session.UserID], function(error,result,fields){
+            if (result && result.length > 0) {
+                console.log(result);
+                for (let i=0; i < result.length; i++) {
+                    const temp = {
+                        id: result[i].Gameid,
+                        course: result[i].GameTitle,
+                        income: result[i].Income
+                    }
+                    contents.push(temp);
+                }
+            }
+
+            return res.status(200).send({ status: "success", data: contents })
+        });
+    }
+})
+
+router.post('/profile', async function(req, res) {
+    const data = req.body;
+    if (req.session === undefined || !req.session.loggedin) {
+        return res.status(401).send({ status: "error", message: "Not logged in" });
+    } else {
+        const params = [
+            data.name, data.surname, data.email, data.username, data.email
+        ];
+
+        await connection.query('UPDATE user SET name = ?, surname = ?, email = ?, username = ? WHERE email = ?', params, async function(error,result,fields){
+            if (result && result.affectedRows && result.affectedRows === 1) {
+                let retMsg = "Updated user successfully.";
+                if (data.password !== "") {
+                    await connection.query('UPDATE user SET password = ? WHERE email = ?', [data.password, data.email], function(error,result,fields){
+                        console.log("Updating password");
+                        if (result && result.affectedRows && result.affectedRows === 1) {
+                            retMsg += " Password updated.";
+                        }
+                        return res.status(200).send({ status: "success", message: retMsg })
+                    });
+                } else {
+                    return res.status(200).send({ status: "success", message: retMsg })
+                }
+            }else {
+                return res.status(200).send({ status: "error", message: "User data not updated" });
+            }
+        });
+    }
+});
+
+router.get('/feedback/list', async function(req, res) {
+    if (req.session === undefined || !req.session.loggedin) {
+        return res.status(401).send({ status: "error", message: "Not logged in" });
+    } else {
+        const feedbackList = [];
+        await connection.query('SELECT f.*, g.* FROM feedback f LEFT JOIN GAMES g ON f.gameId = g.idGames', [], function(error,result,fields){
+            if (result.length > 0) {
+                for (let i = 0; i < result.length; i++) {
+                    const temp = {
+                        id: result[i].id,
+                        title: result[i].GameTitle,
+                        message: result[i].feedbackMessage,
+                        rating: result[i].rating,
+                        timestamp: result[i].timestamp
+                    }
+                    feedbackList.push(temp);
+                }
+            }
+
+            return res.status(200).send({ status: "success", data: feedbackList })
+        });
+    }
+})
 
 module.exports = router;
